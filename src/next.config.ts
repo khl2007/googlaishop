76f49bf4ee53f3576273983ee5e55115fec1210b
khalil qandil <khalilqandil@gmail.com>
2025-07-01 07:53:08 +0000

    i need to clean cash

---
+++ src/next.config.ts
@@ -0,0 +1,22 @@
+
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
